3323abf6486daaa89f03d2a9fcf8cd9debb1c1fa
JaeHoonYun <wogns8030@naver.com>
2023-10-25 12:23:55 +0900

    수정

---
+++ Menu.java
@@ -0,0 +1,26 @@
+package NBCassignment.JH;
+
+public class Menu {
+    private String name;
+    private String explanation;
+    public Menu(String name, String explanation){
+        this.name = name;
+        this.explanation = explanation;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getExplanation(){
+        return explanation;
+    }
+    public void setName(String name, String explanation){
+        this.name = name;
+        this.explanation = explanation;
+    }
+    public void setExplanation(String name, String explanation){
+        this.name = name;
+        this.explanation = explanation;
+    }
+
+
+}
